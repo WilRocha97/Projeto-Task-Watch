2dadda864b27bcd5a24b51deae833e7ac0d9eb25
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2024-08-13 15:30:09 -0300

    Atualização
    Otimizações pontuais no CSS e adição da API para verificar o tema do navegador e saber se deve abrir o site no modo claro ou escuro

--- js/modo.js
+++ js/modo.js
@@ -6,3 +6,12 @@ botao.addEventListener('click', (e)=> {
     corpo.classList.toggle('light');
     fundo.classList.toggle('bgLight');
 })
+
+// Verifica se o tema escuro está ativado
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Aplica o tema de acordo com a preferência do usuário
+if (!prefersDarkScheme) {
+    corpo.classList.toggle('light');
+    fundo.classList.toggle('bgLight');
+}
