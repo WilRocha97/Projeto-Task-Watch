2afec0e1e5fa7fa0a5f30cfe50f18828376e6221
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2024-12-17 11:14:25 -0300

    Atualização
    Agora o campo de porcentagem tem um fundo dinâmico

--- js/temas.js
+++ js/temas.js
@@ -1,9 +1,9 @@
 export function decoracao(card) {
     // Escolhe uma imagem aleatória
-    const {periodo, feriado} = definePeriodo()
+    const {variantesImagens, feriado} = definevariantesImagens()
 
-    if (periodo > 0) {
-        const imagemAleatoria = [Math.floor(Math.random() * periodo)];
+    if (variantesImagens > 0) {
+        const imagemAleatoria = [Math.floor(Math.random() * variantesImagens)];
         // Atualiza a imagem
         card.querySelector('#decoracao').src = `assets/temas/${feriado}/${imagemAleatoria}.png`;
     }
@@ -13,34 +13,34 @@ export function decoracao(card) {
     
 }
 
-function definePeriodo() {
+function definevariantesImagens() {
     const hoje = new Date();
     const dia = hoje.getDate();
     const mes = hoje.getMonth() + 1; // Os meses no JavaScript começam em 0
 
-    let periodo;
+    let variantesImagens;
     let feriado;
 
     // Verifica se está entre 1º de dezembro e 10 de janeiro
     if ((mes === 12 && dia >= 1) || (mes === 12 && dia <= 31)) {
-        periodo = 8;
+        variantesImagens = 8;
         feriado = 'natal'
     } 
     else if ((mes === 1 && dia >= 1) || (mes === 1 && dia <= 2)) {
-        periodo = 8;
+        variantesImagens = 8;
         feriado = 'anoNovo'
     }
     else if ((mes === 4 && dia >= 1) || (mes === 4 && dia <= 30)) {
-        periodo = 6;
+        variantesImagens = 6;
         feriado = 'pascoa'
     }
     else if ((mes === 10 && dia >= 1) || (mes === 10 && dia <= 31)) {
-        periodo = 19;
+        variantesImagens = 19;
         feriado = 'halloween'
     }
     else {
-        periodo = 0; // Outro valor padrão, pode ser ajustado conforme necessário
+        variantesImagens = 0; // Outro valor padrão, pode ser ajustado conforme necessário
     }
 
-    return {periodo, feriado};
+    return {variantesImagens, feriado};
 }
