a37262bc8e6101e627540b88ad1437e789735d99
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2025-07-29 15:45:11 -0300

    Otimização
    Nova animação no botão de troca de temas do site

--- js/modo.js
+++ js/modo.js
@@ -3,22 +3,36 @@ const botao = document.getElementById("toggleDarkMode")
 const imagem = botao.querySelector('img');
 
 botao.addEventListener('click', (e)=> {
+    // Adiciona a animação de saída
+    imagem.classList.remove('anim-in');
+    imagem.classList.add('anim-out');
+
     corpo.classList.toggle('light');
     corpo.classList.toggle('dark');
-    if (corpo.classList.contains('light')) {
-        // Atualiza o atributo src da imagem
-        imagem.src = 'assets/sun.png';
-        // Atualiza o atributo alt da imagem
-        imagem.alt = 'Modo claro';
-        botao.title = 'Mudar para modo escuro'
-    }
-    else {
-        // Atualiza o atributo src da imagem
-        imagem.src = 'assets/moon.png';
-        // Atualiza o atributo alt da imagem
-        imagem.alt = 'Modo escuro';
-        botao.title = 'Mudar para modo claro'
-    }
+
+    // Espera a animação de saída terminar para trocar a imagem
+    imagem.addEventListener('animationend', function handleOut() {
+        imagem.removeEventListener('animationend', handleOut); // Remove o listener
+        
+        if (corpo.classList.contains('light')) {
+            // Atualiza o atributo src da imagem
+            imagem.src = 'assets/sun.png';
+            // Atualiza o atributo alt da imagem
+            imagem.alt = 'Modo claro';
+            botao.title = 'Mudar para modo escuro'
+        }
+        else {
+            // Atualiza o atributo src da imagem
+            imagem.src = 'assets/moon.png';
+            // Atualiza o atributo alt da imagem
+            imagem.alt = 'Modo escuro';
+            botao.title = 'Mudar para modo claro'
+        }
+
+        // Adiciona a animação de entrada
+        imagem.classList.remove('anim-out');
+        imagem.classList.add('anim-in');
+    });
 })
 
 // Verifica se o tema escuro está ativado
