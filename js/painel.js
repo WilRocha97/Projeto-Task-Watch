1733f36d0a08acf2d1878c0aabf4cbeb63c8f4fa
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2025-12-22 10:18:57 -0300

    Atualização
    Função que ajusta o visual das listas do painel conforme elas são atualizadas (Só é chamada na versão de produção do TaskWatch)

--- js/painel.js
+++ js/painel.js
@@ -13,6 +13,32 @@ const corOciosoSombra = styles.getPropertyValue('--cor-ocioso').trim();
 
 const ctx = document.getElementById('meuDonut');
 
+function verificaListasAbertas() {
+    const elements = Array.from(document.querySelector('#targetPainel').children);
+  
+    elements.forEach((element, index) => {
+        if (!element.classList.contains('chartBox')) {
+            // Remove classes anteriores
+            element.classList.remove('aD', 'aE', 'aED');
+            
+            // Verifica se algum elemento depois não tem 'invisible5'
+            const hasVisibleAfter = elements.slice(index + 1).some(el => !el.classList.contains('invisible5'));
+            
+            // Verifica se algum elemento antes não tem 'invisible5'
+            const hasVisibleBefore = elements.slice(1, index).some(el => !el.classList.contains('invisible5'));
+            
+            // Aplica as classes conforme a lógica
+            if (hasVisibleBefore && hasVisibleAfter) {
+            element.classList.add('aED');
+            } else if (hasVisibleAfter) {
+            element.classList.add('aD');
+            } else if (hasVisibleBefore) {
+            element.classList.add('aE');
+            }
+        }
+    });
+}
+
 new Chart(ctx, {
     type: 'doughnut',
     data: {
@@ -50,4 +76,3 @@ new Chart(ctx, {
         },
     }
 });
-
